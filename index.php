31eef48e43d22226a01c69fe5dc9e7c066bb16b6
Alex Cesar Lopes <aclopes@gmail.com>
2014-08-17 23:41:01 -0300

    colocado as chaves do menu circular ainda não terminado

--- index.php
+++ index.php
@@ -12,14 +12,22 @@ and open the template in the editor.
         <title>Sistema de Cadastros Escola</title>
     </head>
     <body>
-        <div id="header">Escola</div>
-        <nav id="menuP">
-             <div class="botao"><a href="cadastro_aluno_form.php">Cadastro Aluno</a></div>
-             <div class="botao"><a href="listagem_aluno.php">Listagem Aluno</a></div>
-             <div class="botao"><a href="cadastro_professor_form.php">Cadastro Professor</a></div>
-             <div class="botao"><a href="listagem_professor_param.php" target="_blank">Listagem Professor</a></div>
-             <div class="botao"><a href="cadastro_curso_form.php">Cadastro Curso</a></div>
-             <div class="botao"><a href="cadastro_matricula_form.php">Cadastro de Matricula</a></div>
+        <div id="header">
+            Escola
+            <div><a href="https://github.com/Aclopes" class="fa fa-github fa-2x"></a></div>
+        </div>
+        <nav class="circular-menu">
+            <div class="circle">
+                <a href="index.php" class="fa fa-home fa-2x">Home</a>
+                <a href="cadastro_aluno_form.php" >Cadastro Aluno</a>
+                <a href="listagem_aluno.php">Lista Alunos</a>
+                <a href="cadastro_professor_form.php">Cadastro Professor</a>
+                <a href="listagem_professor_param.php" target="_blank">Listagem Professor</a>
+                <a href="cadastro_curso_form.php">Cadastro Curso</a>
+                <a href="cadastro_matricula_form.php">Cadastro de Matricula</a>
+                <a href="" class="fa fa-linkedin fa-2x"></a>
+            </div>
+             <a href="" class="menu-button fa fa-bars fa-2x"></a>
         </nav>
     </body>
 </html>
---
+++ js/menu.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Demo by http://creative-punch.net
+
+var items = document.querySelectorAll('.circle a');
+
+for(var i = 0, l = items.length; i < l; i++) {
+  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+  
+  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+}
+
+document.querySelector('.menu-button').onclick = function(e) {
+   e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+};
+
